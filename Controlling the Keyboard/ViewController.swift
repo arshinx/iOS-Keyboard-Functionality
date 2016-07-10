e21bc90c716ad6964252ef1777823fd5b58fdd36
Arshin Jain <arshin.jain@outlook.com>
2016-07-10 07:57:34 -0700

    Keyboard Mod

--- Controlling the Keyboard/ViewController.swift
+++ Controlling the Keyboard/ViewController.swift
@@ -27,6 +27,10 @@ class ViewController: UIViewController {
         
         
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 
 
 }
